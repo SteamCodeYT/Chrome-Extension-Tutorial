792e796b9dfe39216faa9254a38447d6da758021
SteamCode <77702498+SteamCodeYT@users.noreply.github.com>
2022-05-12 11:31:19 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -1,22 +1,30 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("hello")
+
+    //receiving a message
+    chrome.runtime.onMessage.addListener(
+        function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+                    "from a content script:" + sender.tab.url :
+                    "from the extension");
+        if (request.greeting === "hello")
+            sendResponse({farewell: "goodbye"});
+        }
+    );
+
+    //create context menu
+    chrome.contextMenus.create({
+        id: "wikipedia",
+        title: "Search for: \"%s\" on Wikipedia", 
+        contexts: ["selection"], 
+    })
 });
 
-//receiving a message
-chrome.runtime.onMessage.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      if (request.greeting === "hello")
-        sendResponse({farewell: "goodbye"});
-    }
-  );
-
-
-//create context menu
-chrome.contextMenus.create({
-    id: "1",
-    title: "You selected \"%s\"", 
-    contexts: ["selection"], 
+//listener for context menu
+chrome.contextMenus.onClicked.addListener(function(info, tab){
+    //the URL that will be added to based on the selection
+    baseURL = "http://en.wikipedia.org/wiki/";
+    var newURL = baseURL + info.selectionText;
+    //create the new URL in the user's browser
+    chrome.tabs.create({ url: newURL });
 })
