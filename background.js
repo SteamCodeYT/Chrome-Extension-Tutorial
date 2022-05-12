95a19eb42f7bfd28b2fcdec909ae3244a119f6c9
SteamCode <77702498+SteamCodeYT@users.noreply.github.com>
2022-05-11 19:34:02 -0500

    Add files via upload

--- background.js
+++ background.js
@@ -1,3 +1,22 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("hello")
 });
+
+//receiving a message
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      console.log(sender.tab ?
+                  "from a content script:" + sender.tab.url :
+                  "from the extension");
+      if (request.greeting === "hello")
+        sendResponse({farewell: "goodbye"});
+    }
+  );
+
+
+//create context menu
+chrome.contextMenus.create({
+    id: "1",
+    title: "You selected \"%s\"", 
+    contexts: ["selection"], 
+})
